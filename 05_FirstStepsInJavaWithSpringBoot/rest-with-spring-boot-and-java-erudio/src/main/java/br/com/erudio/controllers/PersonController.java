ad0436f821d1fb7111515f0a203613a366c35a2d
Rodrigo Felix <rodrigo.felix321@gmail.com>
2022-08-28 17:10:43 -0300

    Versionamento de endpoints

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.services.PersonServices;
 
 @RestController
@@ -41,6 +42,13 @@ public class PersonController {
 	public PersonVO create(
 			@RequestBody PersonVO PersonVO) {
 		return service.create(PersonVO);
+	}
+	
+	@PostMapping(value = "/v2", consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVOV2 createV2(
+			@RequestBody PersonVOV2 PersonVOV2) {
+		return service.createV2(PersonVOV2);
 	}
 	
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
